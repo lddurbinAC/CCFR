d30d18ebd7be8bb44264fc185d874f313dffd382
lddurbin <l.d.durbin@gmail.com>
2022-07-01 23:08:25 +1200

    Added a function for counting facilities by delivery model. Encoded the colours.

--- scripts/ltp_explainer.R
+++ scripts/ltp_explainer.R
@@ -11,8 +11,17 @@ choose_facilities <- function(local_board) {
     mutate(coord_x = (row_number()-1) %% 6, coord_y = floor((row_number()/6)-0.1))
 }
 
+count_facilities <- function(local_board, delivery_model) {
+  facilities |> 
+    count(local_board, delivery_model) |> 
+    filter(local_board == {{local_board}} & delivery_model == {{delivery_model}}) |> 
+    pull(n)
+}
+
 build_visual <- function(facilities_blocks) {
-  cols <- c("council led" = "#00304B", "community led" = "#6E963C")
+  cols <- c("council led" = "#007CB9", "community led" = "#6E963C")
+  community_count <- count_facilities(facilities_blocks$local_board[[1]], "community led")
+  council_count <- count_facilities(facilities_blocks$local_board[[1]], "council led")
   
   ggplot(data = facilities_blocks, aes(x = coord_x, y = -coord_y)) +
     geom_point(aes(colour = delivery_model),shape=15,size=27) +
@@ -21,23 +30,16 @@ build_visual <- function(facilities_blocks) {
     scale_y_continuous(limits = c(-5.5,0.25)) +
     scale_colour_manual(values = cols) +
     theme_void() +
-    theme(legend.position = "none") +
-    ggtitle(paste0("There are XX facilities in the ", facilities_blocks$local_board, " Local Board, listed below"))
+    ggtitle(paste0("There are ", community_count, " <span style='color:", cols[["community led"]], "'><strong>community-led</strong></span> facilities and\n", council_count, " <span style='color:", cols[["council led"]], "'><strong>Council-led</strong></span> facilities in this Local Board")) +
+    theme(
+      legend.position = "none",
+      plot.title = element_markdown(lineheight = 1.1, size = 16, colour = "#515555")
+    ) 
 }
 
 path <- here::here("data")
 
 facilities <- readRDS(fs::dir_ls(path, glob = "*.rds")) |> filter(local_board != "Great Barrier")
 
-choose_facilities("Franklin") |> build_visual()
-
-# ggsave(
-#   here::here("plots/patchwork.png"),
-#   plot = plot,
-#   device = "png",
-#   bg = "white",
-#   dpi = 300,
-#   height = 10,
-#   width = 15
-#   )
+plot <- choose_facilities("Franklin") |> build_visual()
 
