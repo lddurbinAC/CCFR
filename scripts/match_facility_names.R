1a58704250da10c8279042549705f52556fd260a
lddurbin <l.d.durbin@gmail.com>
2022-08-02 16:37:39 +1200

    Read in the three data files from sync'd SharePoint document library: VH, AC, and CP.

---
+++ scripts/match_facility_names.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(readxl)
+
+# provide your username here
+username <- "durbinl"
+
+# make sure you have the SharePoint File Storage sync'd to your machine
+sharepoint_path <- paste0("C:/Users/", username, "/Auckland Council/CC Insights & Analysis Team - File Storage")
+
+vh <- read_excel(path = paste0(sharepoint_path, "/VH_data.xlsx"))
+ac <- read_excel(path = paste0(sharepoint_path, "/AC SharePoint data.xlsx"), sheet = "A&C SharePoint datafeed")
+cp <- read_excel(path = paste0(sharepoint_path, "/CP_Access_data.xlsx"), sheet = "CP Access")
+
+
