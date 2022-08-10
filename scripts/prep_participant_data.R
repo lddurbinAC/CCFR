f8222d40371218e9364487dfbce47b2ff0feb2aa
lddurbin <l.d.durbin@gmail.com>
2022-08-10 14:25:04 +1200

    Finish data prep before aligning with CCPFR

--- scripts/prep_participant_data.R
+++ scripts/prep_participant_data.R
@@ -7,6 +7,9 @@ source("scripts/functions.R")
 packages <- c("dplyr", "stringr", "purrr", "lubridate")
 get_started(packages)
 
+
+# Functions for this script -----------------------------------------------
+
 # load the desired sheets from the three Excel files as a named list (assuming nobody has these files open!)
 get_data_files <- function() {
   files <- c("VH_data", "AC SharePoint data", "CP_Access_data") # list the Excel files we want to read
@@ -20,6 +23,20 @@ get_data_files <- function() {
   ) |> saveRDS(here::here("data/vh_cpAccess_AC.rds"))
 }
 
+# select columns from the data frame
+col_selection <- function(df, additional_cols, data_src) {
+    df |> 
+    mutate(source = data_src) |> 
+    select(
+      facility_name,
+      month,
+      quarter,
+      year,
+      source,
+      additional_cols
+    )
+}
+
 
 # Load data ---------------------------------------------------------------
 
@@ -27,7 +44,7 @@ get_data_files <- function() {
 get_packages(c("readxl", "here"))
 ccpfr_names <- readxl::read_excel(here::here("data/ccpfr_data/names.xlsx"))
 
-# uncomment the next line if we need to read in the Excel files again
+# *** uncomment the next line if we need to read in the Excel files again ***
 # get_data_files()
 
 # read the data as stored in the rds file
@@ -40,81 +57,30 @@ data <- readRDS(here::here("data/vh_cpAccess_AC.rds"))
 cp_access <- data$CP_Access_data |> 
   mutate(
     month = word(reporting_month, 1, sep=fixed("-")),
-    date_placeholder = dmy(paste0("1 ", month, " 2022")),
-    days_in_month = days_in_month(date_placeholder),
-    month_as_number = month(date_placeholder),
-    quarter = case_when(
-      month_as_number %in% c(7,8,9) ~ "Q1",
-      month_as_number %in% c(10,11,12) ~ "Q2",
-      month_as_number %in% c(1,2,3) ~ "Q3",
-      month_as_number %in% c(4,5,6) ~ "Q4"
-    ),
+    month_as_number = match(month, month.abb),
+    quarter = paste0("Q", quarter(month_as_number, fiscal_start = 7)),
     year_as_number = word(reporting_month, 2, sep=fixed("-")) |> as.double(),
-    previous_year = year_as_number-1,
-    next_year = year_as_number+1,
     year = if_else(
       month_as_number < 7,
-      paste0("FY", previous_year, "/", year_as_number),
-      paste0("FY", year_as_number, "/", next_year)
+      paste0("FY", year_as_number-1, "/", year_as_number),
+      paste0("FY", year_as_number, "/", year_as_number+1)
       ),
     facility_name = word(facility_name_room_name, 1, sep = fixed("-")) |> str_trim(),
     room_name = word(facility_name_room_name, 2, sep = fixed("-")) |> str_trim(),
-    average_hours_per_week = (booked_hrs/days_in_month)*7,
-    utilisation = booked_hrs/(days_in_month*10),
-    source = "CP Access"
+    average_hours_per_week = (booked_hrs/days_in_month(month_as_number))*7,
+    utilisation = booked_hrs/(days_in_month(month_as_number)*10),
     ) |> 
-  select(
-    facility_name,
-    room_name,
-    month,
-    quarter,
-    year,
-    average_hours_per_week,
-    utilisation
-    )
+  col_selection(c("room_name", "average_hours_per_week", "utilisation"), "CP Access")
 
-data$VH_data |> 
-  select(
-    facility_name,
-    room_name = sub_facility_name,
-    month = month_mm,
-    quarter,
-    year = fin_year,
-    attendees,
-    booking_hours)
-
-data$`AC SharePoint data` |> 
+vh <- data$VH_data |> 
   mutate(
-    month = word(month, 2, sep = fixed(" - "))
+    utilisation = booking_hours/gross_standard_available_hours,
+    month = month_mm,
+    year = fin_year,
+    room_name = sub_facility_name
   ) |> 
-  select(partner_name, month, quarter, year, total_attendees_participants)
+  col_selection(c("room_name", "attendees", "booking_hours", "average_hours_per_week", "utilisation", "source"), "VH")
 
-
-# John's requirements...
-# 
-# -Partner or Facility name:
-#   
-#   as per the CCFPR matching with the raw data facility columns
-# 
-# -Local Board:
-#   
-#   For both VH data and AC Sharepoint data - we'll use CCPFR Local Board data instead for consistency, even though LB is already provided in the raw data.
-# 
-# 
-# -Average Hours Per Week:
-#   For VH data -  as per the "Average Hours per week" raw data column, so no further calculations needed
-# For AC Sharepoint data - there is NO Average Hours raw data collected, so this column isn't used for AC data
-# 
-# 
-# 
-# -Utilisation:
-# For VH data - calculated using the  "Booking Hours" raw data column and the "Gross Standard Available Hours" raw data column (using the calculation
-# 
-# Booking Hours/Gross Standard Available Hours)
-# For AC Sharepoint data - NO raw data collected so this column isn't used for AC data
-# 
-# 
-# 
-# -Room: eventually we'd use the space data from the CCPFR for consistency but in the meanwhile:
-# For VH data - as per the "Sub Facility Name" raw data column
-# For AC Sharepoint data - NO raw data collected so this column isn't used for AC data
+ac <- data$`AC SharePoint data` |> 
+  mutate(month = word(month, 2, sep = fixed(" - ")), facility_name = partner_name) |> 
+  col_selection(c("total_attendees_participants"), "AC")
