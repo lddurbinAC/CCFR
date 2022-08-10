4f76377717bf6b8c388ce3bae42d27b3237e9c55
lddurbin <l.d.durbin@gmail.com>
2022-08-11 11:44:58 +1200

    Removed Pakuranga Leisure Centre from VH data

--- scripts/prep_participant_data.R
+++ scripts/prep_participant_data.R
@@ -93,7 +93,9 @@ cp_access |> filter(is.na(primary_name)) |> distinct(facility_name)
 
 # prepare the Venue Hire data
 vh <- get_named_item("VH_data") |> 
-  filter(facility_name != "Unknown") |> 
+  filter(
+    !facility_name %in% c("Unknown", "Pakuranga Leisure Centre")
+    ) |> 
   mutate(
     utilisation = booking_hours/gross_standard_available_hours,
     month = month_mm,
