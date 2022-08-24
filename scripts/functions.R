e176a85dfdd4f031dc573c655c6e1649d9a1c8a3
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2022-08-25 09:25:41 +1200

    Replace read_excel with wrapper function from awhina package

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,14 +1,3 @@
-# **HELPERS PACKAGE: exported function**
-# read Excel an file from SharePoint File Storage, optionally name a sheet, skip rows, and change file path
-get_excel_data <- function(filename, sheetname = "Sheet1", skip_rows = 0, path = get_file_storage_path("SHAREPOINT_FILE_STORAGE")) {
-  readxl::read_excel(
-    path = paste0(path, filename, ".xlsx"),
-    sheet = sheetname,
-    skip = skip_rows,
-    .name_repair = janitor::make_clean_names
-  )
-}
-
 # **CCPFR PACKAGE**
 # join a facilities table with its attributes, returning only facilities
 get_attributes <- function(db_table) {
--- scripts/names.R
+++ scripts/names.R
@@ -6,10 +6,11 @@ library(stringr)
 source("scripts/functions.R")
 
 get_data <- function(file_name, columns) {
-  readxl::read_excel(
-    path = paste0(path, "/", file_name),
-    .name_repair = janitor::make_clean_names
-  ) |> 
+  get_excel_file(filename = file_name, path = path) |> 
+  # readxl::read_excel(
+  #   path = paste0(path, "/", file_name),
+  #   .name_repair = janitor::make_clean_names
+  # ) |> 
     select(facility_name = {{columns}})
 }
 
@@ -24,9 +25,9 @@ packages <- c("dplyr", "readxl")
 get_started(packages)
 path <- get_file_storage_path("SHAREPOINT_FILE_STORAGE")
 
-CP_facilities_unmatched <- get_data("CP Access_Facilities.xlsx", "facility_name") |> match_ccpfr()
-AC_partners_unmatched <- get_data("AC Reporting_Partners.xlsx", "partner_name") |> match_ccpfr()
-CP_rooms_unmatched <- get_data("CP Access_Rooms.xlsx", "facility_name") |> 
+CP_facilities_unmatched <- get_data("/CP Access_Facilities", "facility_name") |> match_ccpfr()
+AC_partners_unmatched <- get_data("/AC Reporting_Partners", "partner_name") |> match_ccpfr()
+CP_rooms_unmatched <- get_data("/CP Access_Rooms", "facility_name") |> 
   mutate(
     facility = word(facility_name, 1, sep = fixed("-")) |> str_squish(),
     facility_name = word(facility_name, 2, sep = fixed("-")) |> str_squish()
